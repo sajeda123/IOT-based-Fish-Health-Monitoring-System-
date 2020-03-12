66162d6a08f96beb1693d6033de27205169b1dfe
Sajeda <61319676+sajeda123@users.noreply.github.com>
2020-03-12 00:43:59 -0700

    Add files via upload

---
+++ Automatic_fisher_system.ino
@@ -0,0 +1,98 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 5
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+ float Celcius=0;
+ float Fahrenheit=0;
+const int analogInPin = A0; 
+int sensorValue = 0; 
+unsigned long int avgValue; 
+float b;
+int buf[10],temp;
+const int read = A0; 
+int value;
+      
+
+void setup() {
+ Serial.begin(9600);
+}
+ 
+void loop() {
+ for(int i=0;i<10;i++) 
+ { 
+  buf[i]=analogRead(analogInPin);
+  delay(10);
+ }
+ for(int i=0;i<9;i++)
+ {
+  for(int j=i+1;j<10;j++)
+  {
+   if(buf[i]>buf[j])
+   {
+    temp=buf[i];
+    buf[i]=buf[j];
+    buf[j]=temp;
+   }
+  }
+ }
+ avgValue=0;
+ for(int i=2;i<8;i++)
+ avgValue+=buf[i];
+ float pHVol=(float)avgValue*5.0/1024/6;
+ float phValue = -5.70 * pHVol + 21.34;
+ Serial.print("sensor = ");
+ Serial.println(phValue);
+ 
+ delay(20);
+
+
+ // For water level sensor //
+ value = analogRead(read); //Read data from analog pin and store it to value variable
+ 
+  if (value<=480){ 
+    Serial.println("Water level: 0mm - Empty!"); 
+  }
+  else if (value>480 && value<=530){ 
+    Serial.println("Water level: 0mm to 5mm"); 
+  }
+  else if (value>530 && value<=615){ 
+    Serial.println("Water level: 5mm to 10mm"); 
+  }
+  else if (value>615 && value<=660){ 
+    Serial.println("Water level: 10mm to 15mm"); 
+  } 
+  else if (value>660 && value<=680){ 
+    Serial.println("Water level: 15mm to 20mm"); 
+  }
+  else if (value>680 && value<=690){ 
+    Serial.println("Water level: 20mm to 25mm"); 
+  }
+  else if (value>690 && value<=700){ 
+    Serial.println("Water level: 25mm to 30mm"); 
+  }
+  else if (value>700 && value<=705){ 
+    Serial.println("Water level: 30mm to 35mm"); 
+  }
+  else if (value>705){ 
+    Serial.println("Water level: 35mm to 40mm"); 
+  }
+  
+  delay(5000);
+
+  // For water temperature sensor //
+    sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  Fahrenheit=sensors.toFahrenheit(Celcius);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  Serial.print(" F  ");
+  Serial.println(Fahrenheit);
+  delay(1000);
+
+  
+}
